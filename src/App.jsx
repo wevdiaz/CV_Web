37bf1f48d630eed5c7d0c4895e939b93825fc0cd
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-08-18 18:16:03 -0300

    add componentes e imagem

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,15 @@
+import { Sidebar } from "./components/Sidebar";
+import { MainContent } from "./components/MainContent";
 
+import "./styles/components/app.sass";
 
 function App() {
   
   return (
-    <div className="App">
-      <h1>React + Sass</h1>
-      <p>Projeto prático</p>
+    <div className="App" id="curriculum">
+      <h1>Weverson Diazz</h1>
+      <Sidebar />
+      <MainContent />
     </div>
   )
 }
---
+++ src/components/MainContent.jsx
@@ -0,0 +1,7 @@
+
+
+export function MainContent() {
+  return (
+    <div>MainContent</div>
+  );
+}
