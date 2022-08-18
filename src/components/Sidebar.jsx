5f49a4b734051d556943f3fe78ae9b94a6b01130
Weverson Diazz <63312710+wevdiaz@users.noreply.github.com>
2022-08-18 19:28:51 -0300

    crando o sidebar

--- src/components/Sidebar.jsx
+++ src/components/Sidebar.jsx
@@ -1,7 +1,17 @@
+import Avatar from "../images/photo.jpeg";
 
+import "../styles/components/sidebar.sass";
 
 export function Sidebar() {
   return (
-    <div>Sidebar</div>
+    <aside id="sidebar">
+      <p>Imagem</p>
+      <p className="title">Desenvolvedor</p>
+      <p>redes sociais</p>
+      <p>informações de contato</p>
+      <a href="#" className="btn">
+        download curriculo
+      </a>
+    </aside>
   );
 }
